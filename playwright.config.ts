ccb9dafbe8a2915bdf05f6f5862e5596036e6009
Mike <mr.mikesorrentino@gmail.com>
2024-10-30 08:45:48 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,9 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:3000',
+		baseURL: process.env.VERCEL_URL
+			? `https://${process.env.VERCEL_URL}`
+			: `http://localhost:${process.env.PORT ?? 3000}`,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -72,9 +74,10 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-    command: 'npm run dev',
-    timeout: 120 * 1000,
-		url: 'http://localhost:3000',
-		reuseExistingServer: !process.env.CI,
+    command: 'npm run start',//'npm run dev',
+    port: 3000,
+		timeout: 120 * 1000,
+		//url: 'http://localhost:3000',
+		reuseExistingServer: true,//!process.env.CI,
 	},
 });
